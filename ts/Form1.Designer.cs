188783dd23581bfde2259a923aab50db1ba0544a
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2024-10-09 11:41:32 +0300

    Add files via upload

---
+++ ts/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            checkBox1 = new CheckBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            showButton = new Button();
+            clearButton = new Button();
+            backgroundButton = new Button();
+            closebutton = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            colorDialog1 = new ColorDialog();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(checkBox1, 0, 1);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 1, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.Size = new Size(784, 561);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            tableLayoutPanel1.SetColumnSpan(pictureBox1, 2);
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(778, 498);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(3, 507);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(59, 19);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Strech";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(showButton);
+            flowLayoutPanel1.Controls.Add(clearButton);
+            flowLayoutPanel1.Controls.Add(backgroundButton);
+            flowLayoutPanel1.Controls.Add(closebutton);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.RightToLeft;
+            flowLayoutPanel1.Location = new Point(120, 507);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(661, 51);
+            flowLayoutPanel1.TabIndex = 2;
+            flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
+            // 
+            // showButton
+            // 
+            showButton.AutoSize = true;
+            showButton.Location = new Point(583, 3);
+            showButton.Name = "showButton";
+            showButton.Size = new Size(75, 25);
+            showButton.TabIndex = 0;
+            showButton.Text = "Show";
+            showButton.UseVisualStyleBackColor = true;
+            showButton.Click += showButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.AutoSize = true;
+            clearButton.Location = new Point(463, 3);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(114, 25);
+            clearButton.TabIndex = 1;
+            clearButton.Text = "Clear the picture";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // backgroundButton
+            // 
+            backgroundButton.AutoSize = true;
+            backgroundButton.Location = new Point(298, 3);
+            backgroundButton.Name = "backgroundButton";
+            backgroundButton.Size = new Size(159, 25);
+            backgroundButton.TabIndex = 2;
+            backgroundButton.Text = "Set the background color";
+            backgroundButton.UseVisualStyleBackColor = true;
+            backgroundButton.Click += backgroundButton_Click;
+            // 
+            // closebutton
+            // 
+            closebutton.AutoSize = true;
+            closebutton.Location = new Point(217, 3);
+            closebutton.Name = "closebutton";
+            closebutton.Size = new Size(75, 25);
+            closebutton.TabIndex = 3;
+            closebutton.Text = "Close";
+            closebutton.UseVisualStyleBackColor = true;
+            closebutton.Click += closeButton_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select a picture file";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 561);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "PictureViewer";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox1;
+        private CheckBox checkBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button showButton;
+        private Button clearButton;
+        private Button backgroundButton;
+        private Button closebutton;
+        private OpenFileDialog openFileDialog1;
+        private ColorDialog colorDialog1;
+    }
+}
---
+++ ts/StartForm.cs
@@ -0,0 +1,292 @@
+﻿using Microsoft.VisualBasic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class MainForm : Form
+    {
+        private TreeView tree;
+        private Button btn, btn22;  // Объявляем две отдельные кнопки
+        private Label lbl;
+        private PictureBox pictureBox;
+        private CheckBox chk1, chk2;
+        private TabControl tabControl;
+        private RadioButton radioButton1, radioButton2;
+        private ListBox listBox;
+        private DataGridView dGV;
+        private DataSet dS;
+        private Button btnAddData;
+        private HashSet<Control> addedControls = new HashSet<Control>();
+        private int pictureIndex = 0;
+
+        private string[] rbtn_list = { "Esimene", "Teine", "Kolmas" };
+
+        public MainForm()
+        {
+            this.Text = "Vorm elementidega";
+            this.Width = 800;
+            this.Height = 600;
+
+            tree = new TreeView();
+            TreeNode tn = new TreeNode("Elemendid:");
+            tn.Nodes.Add(new TreeNode("Nupp"));
+            tn.Nodes.Add(new TreeNode("Silt"));
+            tn.Nodes.Add(new TreeNode("Pilt"));
+            tn.Nodes.Add(new TreeNode("Märkeruut"));
+            tn.Nodes.Add(new TreeNode("TabControl"));
+            tn.Nodes.Add(new TreeNode("ListBox"));
+            tn.Nodes.Add(new TreeNode("DataGridView"));
+            tn.Nodes.Add(new TreeNode("Dialogue"));
+            tn.Nodes.Add(new TreeNode("Add Plant"));
+            tn.Nodes.Add(new TreeNode("test"));  // Добавляем test для btn22
+            tree.Nodes.Add(tn);
+            tree.Dock = DockStyle.Left;
+            tree.AfterSelect += Tree_AfterSelect;
+            this.Controls.Add(tree);
+
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
+            lbl = new Label();
+            lbl.Text = "Elementide loomine c# abil";
+            lbl.Font = new Font("Arial", 32, FontStyle.Bold);
+            lbl.Location = new Point(200, 20);
+            lbl.AutoSize = true;
+            lbl.MouseHover += Lbl_MouseHover;
+            lbl.MouseLeave += Lbl_MouseLeave;
+
+            btn = new Button();
+            btn.Text = "3 vorm";
+            btn.Location = new Point(200, 80);
+            btn.Click += Btn_Click;
+
+            btn22 = new Button(); 
+            btn22.Text = "test vorm";
+            btn22.Location = new Point(120,0);  
+            btn22.Click += Btn22_Click;  
+
+            pictureBox = new PictureBox();
+            pictureBox.Image = Image.FromFile(@"..\..\..\cat.jpg");
+            pictureBox.Size = new Size(100, 100);
+            pictureBox.Location = new Point(200, 150);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.DoubleClick += Pbox_DoubleClick;
+
+            chk1 = new CheckBox();
+            chk1.Text = "Valik 1";
+            chk1.Location = new Point(200, 270);
+            chk1.CheckedChanged += Chk_CheckedChanged;
+
+            chk2 = new CheckBox();
+            chk2.Text = "Valik 2";
+            chk2.Location = new Point(200, 300);
+            chk2.CheckedChanged += Chk_CheckedChanged;
+
+            tabControl = new TabControl();
+            tabControl.Location = new Point(600, 100);
+            tabControl.Size = new Size(200, 150);
+            var tabPage1 = new TabPage("Esimene");
+            var tabPage2 = new TabPage("Teine");
+            var tabPage3 = new TabPage("Kolmas");
+            tabControl.TabPages.Add(tabPage1);
+            tabControl.TabPages.Add(tabPage2);
+            tabControl.TabPages.Add(tabPage3);
+
+            listBox = new ListBox();
+            foreach (string item in rbtn_list)
+            {
+                listBox.Items.Add(item);
+            }
+            listBox.Location = new Point(400, 80);
+            listBox.SelectedIndexChanged += Lb_SelectedIndexChanged;
+
+            dS = new DataSet("XML file");
+            dS.ReadXml(@"..\..\..\plant_catalog.xml");
+            dGV = new DataGridView();
+            dGV.Location = new Point(500, 400);
+            dGV.DataSource = dS;
+            dGV.DataMember = "PLANT";
+            dGV.Click += DGV_Click;
+
+            btnAddData = new Button() { Text = "Sisesta taime", Location = new Point(200, 530) };
+            btnAddData.Click += BtnAddData_Click;
+        }
+
+        private void Tree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            switch (e.Node.Text)
+            {
+                case "Nupp":
+                    AddControlIfNotExists(btn);
+                    break;
+
+                case "Silt":
+                    AddControlIfNotExists(lbl);
+                    break;
+
+                case "Pilt":
+                    AddControlIfNotExists(pictureBox);
+                    break;
+
+                case "Märkeruut":
+                    AddControlIfNotExists(chk1);
+                    AddControlIfNotExists(chk2);
+                    break;
+
+                case "TabControl":
+                    AddControlIfNotExists(tabControl);
+                    break;
+
+                case "ListBox":
+                    AddControlIfNotExists(listBox);
+                    break;
+
+                case "DataGridView":
+                    AddControlIfNotExists(dGV);
+                    break;
+
+                case "Dialogue":
+                    DG_Show();
+                    break;
+
+                case "Add Plant":
+                    AddControlIfNotExists(btnAddData);
+                    break;
+
+                case "test":  
+                    AddControlIfNotExists(btn22);
+                    break;
+            }
+        }
+
+        private void AddControlIfNotExists(Control control)
+        {
+            if (!addedControls.Contains(control))
+            {
+                this.Controls.Add(control);
+                addedControls.Add(control);
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kõige lihtsam aken");
+        }
+
+        private void Pbox_DoubleClick(object? sender, EventArgs e)
+        {
+            string[] pildid = { "cat.jpg", "cat2.jpg", "cat3.jpg" };
+            pictureIndex = (pictureIndex + 1) % pildid.Length;
+            pictureBox.Image = Image.FromFile(@"..\..\..\" + pildid[pictureIndex]);
+        }
+
+        private void Chk_CheckedChanged(object? sender, EventArgs e)
+        {
+            if (chk1.Checked && chk2.Checked)
+            {
+                lbl.BorderStyle = BorderStyle.Fixed3D;
+                pictureBox.BorderStyle = BorderStyle.Fixed3D;
+            }
+            else if (chk1.Checked)
+            {
+                lbl.BorderStyle = BorderStyle.Fixed3D;
+                pictureBox.BorderStyle = BorderStyle.None;
+            }
+            else if (chk2.Checked)
+            {
+                pictureBox.BorderStyle = BorderStyle.Fixed3D;
+                lbl.BorderStyle = BorderStyle.None;
+            }
+            else
+            {
+                lbl.BorderStyle = BorderStyle.None;
+                pictureBox.BorderStyle = BorderStyle.None;
+            }
+        }
+
+        private void Lbl_MouseHover(object? sender, EventArgs e)
+        {
+            lbl.Font = new Font("Arial", 32, FontStyle.Underline);
+            lbl.ForeColor = Color.FromArgb(70, 50, 150, 200);
+        }
+
+        private void Lbl_MouseLeave(object? sender, EventArgs e)
+        {
+            lbl.Font = new Font("Arial", 32, FontStyle.Bold);
+        }
+
+        private void Lb_SelectedIndexChanged(object? sender, EventArgs e)
+        {
+            switch (listBox.SelectedIndex)
+            {
+                case 0: tree.BackColor = Color.Red; break;
+                case 1: tree.BackColor = Color.Chocolate; break;
+                case 2: tree.BackColor = Color.Purple; break;
+                case 3: tree.BackColor = Color.PaleTurquoise; break;
+            }
+        }
+
+        private void DGV_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show(dGV.SelectedCells[0].Value.ToString());
+        }
+
+        private void DG_Show()
+        {
+            MessageBox.Show("Dialogue", "This is a simple window");
+            var vastus = MessageBox.Show("Insert data", "Do you want to use the InputBox?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (vastus == DialogResult.Yes)
+            {
+                string text = Interaction.InputBox("Write something here", "Data insertion");
+                MessageBox.Show("You wrote: " + text, text);
+            }
+        }
+
+        private void BtnAddData_Click(object sender, EventArgs e)
+        {
+            DataRow newRow = dS.Tables["PLANT"].NewRow();
+            newRow["COMMON"] = "Tulip";
+            newRow["BOTANICAL"] = "Tulipa";
+            dS.Tables["PLANT"].Rows.Add(newRow);
+        }
+
+        
+        private Button? btnA, btnB, btnC;
+
+        private void Btn22_Click(object sender, EventArgs e)
+        {
+            if (btnA == null && btnB == null && btnC == null)
+            {
+                
+                btnA = new Button();
+                btnA.Text = "A";
+                btnA.Location = new Point(140, 30);
+                btnA.Click += btn222_click;
+                this.Controls.Add(btnA);
+
+                btnB = new Button();
+                btnB.Text = "B";
+                btnB.Location = new Point(240, 30);
+                btnB.Click += (s, args) => { MessageBox.Show("B"); };
+                this.Controls.Add(btnB);
+
+                btnC = new Button();
+                btnC.Text = "C";
+                btnC.Location = new Point(340, 30);
+                btnC.Click += (s, args) => { MessageBox.Show("C"); };
+                this.Controls.Add(btnC);
+
+                this.Refresh();  
+            }
+        }
+
+        private void btn222_click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+    } 
+}
